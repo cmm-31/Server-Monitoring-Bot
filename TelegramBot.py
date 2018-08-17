35c5d54f2be064f1b6835c6b6131188b3d8135c6
Christian Masek <christian.masek@bluewin.ch>
2018-08-17 10:22:21 +0200

    Cleaned the code and changed the result to an actual True or False.

--- TelegramBot.py
+++ TelegramBot.py
@@ -7,32 +7,25 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--token", dest="token", help="set bot token")
-parser.add_argument("-id", "--chat_id", dest="chat_id", help="set chat_id")
+parser.add_argument("-id", "--chat_id", dest="chat_id", help="set the chat_id")
 args = parser.parse_args()
-print(args.token)
-print(args.chat_id)
 
 
 def ping_server(HOST, PORT):
-
-    m = b'{"id": 2, "method": "server.version"}\n'
-
     # Create a socket (SOCK_STREAM means a TCP socket)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         sock.connect((HOST, PORT))
-        sock.sendall(m)
-
+        sock.sendall(b'{"id": 2, "method": "server.version"}\n')
         received = sock.recv(1024)
     finally:
         sock.close()
     version_dict = json.loads(received)
-
-    return bool(version_dict["result"])
-
-
-version = ping_server("electrumx-ch-1.feathercoin.ch", 50001)
+    if "ElectrumX 1.5.1" in version_dict["result"]:
+        return True
+    else:
+        return False
 
 
 def sendMessage(token, chat_id, text):
@@ -40,5 +33,7 @@ def sendMessage(token, chat_id, text):
 
     requests.post(url, data={"chat_id": chat_id, "text": text})
 
+
+version = ping_server("electrumx-ch-1.feathercoin.ch", 50001)
 
 sendMessage(args.token, args.chat_id, version)
