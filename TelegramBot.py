0d25bd35d78340110048953fd72cd1eb500c64a4
Christian Masek <christian.masek@bluewin.ch>
2018-08-24 16:52:17 +0200

    there's now a function main() in which the whole program runs

--- TelegramBot.py
+++ TelegramBot.py
@@ -17,22 +17,26 @@ def sendMessage(token, chat_id, text):
 
 
 class Host():
-    def __init__(self, name, port):
+    def __init__(self, name, port, counter_limit, token, chat_id, recheck_duration_para):
         self.name = name
         self.port = int(port)
         self.state = State.running
         self.counter = 0
         self.recheck_at = datetime.datetime.now()
+        self.counter_limit = counter_limit
+        self.token = token
+        self.chat_id = chat_id
+        self.recheck_duration_para = recheck_duration_para
 
 
-    def goto_state_failed(self):
+    def goto_state_failed(self, name, state, recheck_at, token, chat_id, recheck_duration):
         message = "The following server isn't responding properly. Please check it: " + self.name
-        sendMessage(args.token, args.chat_id, message)
+        sendMessage(self.token, self.chat_id, message)
         self.state = State.failed
-        self.recheck_at = datetime.datetime.now() + datetime.timedelta(**recheck_duration)
+        self.recheck_at = datetime.datetime.now() + datetime.timedelta(**self.recheck_duration_para)
 
 
-    def ping_server(self):
+    def ping_server(self, name, port):
         # Create a socket (SOCK_STREAM means a TCP socket)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -51,29 +55,29 @@ class Host():
         return version_dict["result"][0].startswith("ElectrumX")
 
 
-    def goto_state_running(self):
+    def goto_state_running(self, name, state, counter):
         logging.debug("Logging state is turned on running again for " + self.name)
         self.state = State.running
         self.counter = 0
 
 
-    def log_successful_ping(self):
+    def log_successful_ping(self, name):
         logging.debug("Ping was successful for " + self.name)
 
 
-    def is_state_running(self):
+    def is_state_running(self, state):
         return self.state == State.running
 
 
-    def reached_rechecktime(self):
+    def reached_rechecktime(self, recheck_at):
         return self.recheck_at < datetime.datetime.now()
 
 
-    def reached_max_fails(self):
-        return self.counter == args.counter
+    def reached_max_fails(self, counter, counter_limit):
+        return self.counter == counter_limit
 
 
-    def count_failed_ping(self):
+    def count_failed_ping(self, counter, name):
         self.counter += 1
         logging.debug("Ping was not successful for " + self.name)
 
@@ -85,47 +89,59 @@ class AutoNumber(Enum):
         obj._value_ = value
         return obj
 
+
 class State(AutoNumber):
     running = ()
     failed = ()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", "--token", dest="token", help="set bot token")
-parser.add_argument("-id", "--chat_id", dest="chat_id", help="set the chat_id")
-parser.add_argument("-ho", "--hosts", dest="hosts", help="set the hosts")
-parser.add_argument("-chec", "--recheck_duration", dest="recheck_duration", default="days:1", help="set the time until recheck")
-parser.add_argument("-de", "--debug", dest="debug", default=False, type=bool, help="enable logging.debug if wanted")
-parser.add_argument("-log", "--logfile", dest="logfile", default=False, type=bool, help="logging.debug creates a file")
-parser.add_argument("-c", "--counter", dest="counter", default=5, type=int, help="set the check times, until the msg will be sent")
-args = parser.parse_args()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--token", dest="token", help="set bot token")
+    parser.add_argument("-id", "--chat_id", dest="chat_id", help="set the chat_id")
+    parser.add_argument("-ho", "--hosts", dest="hosts", help="set the hosts")
+    parser.add_argument("-chec", "--recheck_duration", dest="recheck_duration", default="days:1", help="set the time until recheck")
+    parser.add_argument("-de", "--debug", dest="debug", default=False, type=bool, help="enable logging.debug if wanted")
+    parser.add_argument("-log", "--logfile", dest="logfile", default=False, type=bool, help="logging.debug creates a file")
+    parser.add_argument("-c", "--counter", dest="counter", default=5, type=int, help="set the check times, until the msg will be sent")
+    args = parser.parse_args()
+
+    counter_limit = args.counter
+    token = args.token
+    chat_id = args.chat_id
+    recheck_duration_para = recheck_duration
 
 
-if args.debug and args.logfile:
-    logging.basicConfig(filename="Logfile_debug", level=logging.DEBUG)
-elif args.debug:
-    logging.basicConfig(level=logging.DEBUG)
+    if args.debug and args.logfile:
+        logging.basicConfig(filename="Logfile_debug", level=logging.DEBUG)
+    elif args.debug:
+        logging.basicConfig(level=logging.DEBUG)
 
-hosts = []
-for host in args.hosts.split(","):
-    name, port = host.split(":")
-    hosts.append(Host(name, port))
+    recheck_duration = {}
+    for duration in args.recheck_duration.split(","):
+        unit, amount = duration.split(":")
+        recheck_duration[unit] = int(amount)
 
 
-recheck_duration = {}
-for duration in args.recheck_duration.split(","):
-    unit, amount = duration.split(":")
-    recheck_duration[unit] = int(amount)
+    hosts = []
+    for host in args.hosts.split(","):
+        name, port = host.split(":")
+        hosts.append(Host(name, port, counter_limit, token, chat_id, recheck_duration_para))
 
-while True:
-    for host in hosts:
-        success = host.ping_server()
-        if success:
-            host.log_successful_ping()
-        elif host.is_state_running():
-            host.count_failed_ping()
-            if host.reached_max_fails():
-                host.goto_state_failed()
 
-        if host.reached_max_fails() and host.reached_rechecktime():
-            host.goto_state_running()
-    time.sleep(10)
+    while True:
+        for host in hosts:
+            success = host.ping_server(host.name, host.port)
+            if success:
+                host.log_successful_ping(host.name)
+            elif host.is_state_running(host.state):
+                host.count_failed_ping(host.counter, host.name)
+                if host.reached_max_fails(host.counter, host.counter_limit):
+                    host.goto_state_failed(host.name, host.state, host.recheck_at, host.token, host.chat_id, host.recheck_duration_para)
+
+            if host.reached_max_fails(host.counter, host.counter_limit) and host.reached_rechecktime(host.recheck_at):
+                host.goto_state_running(host.name, host.state, host.counter)
+        time.sleep(10)
+
+
+main()
